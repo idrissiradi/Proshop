2146c5bd64707026047a24fd065d0b63e0368425
idrissiradi <idrissiradi@gmail.com>
2021-06-09 17:10:03 +0100

    Start Express server

---
+++ README.md
@@ -0,0 +1,11 @@
+# MERN Shopping cart app
+
+# Getting Started with Create React App
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `yarn start`
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
+import './bootstrap.min.css'
 import './index.css'
 import App from './App'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
 )
