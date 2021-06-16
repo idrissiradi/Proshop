747665b6b00ddaed575fac2d3d5254dbf92bd23c
idrissiradi <idrissiradi@gmail.com>
2021-06-16 13:00:15 +0100

    implementing Redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 
 import './bootstrap.min.css'
 import './index.css'
 import App from './App'
+import store from './store'
 
 ReactDOM.render(
-    <React.StrictMode>
+    <Provider store={store}>
         <App />
-    </React.StrictMode>,
+    </Provider>,
     document.getElementById('root')
 )
