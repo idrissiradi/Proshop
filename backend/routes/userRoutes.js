01eb5668c57f153fb7e1e89d6b1569d68349370f
idrissiradi <idrissiradi@gmail.com>
2021-06-18 17:32:41 +0100

    Back End user Authentication

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import {
+    authUser,
+    registerUser,
+    getUserProfile,
+} from '../controllers/userController.js'
+import { protect } from '../middleware/authMiddleware.js'
+
+const router = express.Router()
+
+router.route('/').post(registerUser)
+router.post('/login', authUser)
+router.route('/profile').get(protect, getUserProfile)
+
+export default router
